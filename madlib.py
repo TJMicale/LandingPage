f8944f09bc48b17ca74f91e8f93c118b28f32da9
TJ Micale <micale.tim@gmail.com>
2022-11-26 09:23:04 -0500

    madlib project started and Tic Tac Toe project

---
+++ madlib.py
@@ -0,0 +1,7 @@
+Color = input("Color: ")
+PluralNoun = input("Plural Noun: ")
+Celebrity = input("Name a Celebrity: ")
+
+print("Roses are", Color)
+print(PluralNoun, "are blue")
+print("I love,",Celebrity)
